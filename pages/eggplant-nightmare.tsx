34bbf287d885a7418433e31f4788bf711c2ff08c
Zack Spellman <zandaleph@gmail.com>
2022-12-20 23:47:01 -0800

    Update to v0.1 of Eggplant Nightmare

--- pages/eggplant-nightmare.tsx
+++ pages/eggplant-nightmare.tsx
@@ -3,47 +3,82 @@ import { NextPage } from "next/types";
 import { type Scene } from "phaser";
 import PhaserComponent from "../components/phaser/PhaserComponent";
 
-function preload(this: Scene) {
-  this.load.setBaseURL("http://labs.phaser.io");
-
-  this.load.image("sky", "assets/skies/space3.png");
-  this.load.image("logo", "assets/sprites/phaser3-logo.png");
-  this.load.image("red", "assets/particles/red.png");
+function preloadScene(this: Scene) {
+  this.load.image("player", "https://i.imgur.com/i2fxxTK.png");
+  this.load.image("background", "https://i.imgur.com/v3VD6rs.png");
+  this.load.image("enemy", "https://i.imgur.com/DOPB0i9.png");
 }
 
-function create(this: Scene) {
-  this.add.image(400, 300, "sky");
+function gameScene(this: Scene) {
+  let background = this.add.tileSprite(500, 500, 1000, 1000, "background");
 
-  var particles = this.add.particles("red");
+  let player = this.add.sprite(100, 100, "player");
 
-  var emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: "ADD",
-  });
+  let enemy = this.add.sprite(1000, Phaser.Math.Between(20, 890), "enemy");
 
-  var logo = this.physics.add.image(400, 100, "logo");
+  this.update = function () {
+    background.tilePositionX += 2;
 
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
+    if (this.input.mousePointer.leftButtonDown()) {
+      player.y -= 5;
+    } else {
+      player.y += 5;
+    }
+    if (player.y < 0) {
+      player.y = 0;
+    }
+    if (player.y > 800) {
+      player.y = 800;
+    }
 
-  emitter.startFollow(logo);
+    enemy.x -= 5;
+    if (enemy.x < 0) {
+      enemy.x = 1000;
+      enemy.y = Phaser.Math.Between(20, 980);
+    }
+    //collision detection
+    let distance = Phaser.Math.Distance.Between(
+      player.x,
+      player.y,
+      enemy.x,
+      enemy.y
+    );
+    if (distance < 200) {
+      enemy.destroy();
+
+      let text = this.add.text(
+        500,
+        500,
+        "Noooo! You have been\nturned into\nEGGPLANT PARMESAN.\nClick to Restart",
+        {
+          fontSize: "64px",
+          align: "center",
+        }
+      );
+
+      text.setOrigin(0.5, 0.5);
+
+      //Restart
+      this.input.once(
+        "pointerdown",
+        function (this: Scene) {
+          this.scene.restart();
+        },
+        this
+      );
+    }
+  };
 }
 
 // Hello world example
 const config: Phaser.Types.Core.GameConfig = {
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 200 },
-    },
-  },
+  //Game dimensions
+  width: 1000,
+  height: 1000,
+  //Game scenes
   scene: {
-    preload: preload,
-    create: create,
+    preload: preloadScene,
+    create: gameScene,
   },
 };
 
